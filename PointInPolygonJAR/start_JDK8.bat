238e334c438df714db7e43d15097a4ea400952ea
Andrew Buldyzhov <54274317+drewbul@users.noreply.github.com>
2020-01-31 17:52:14 +0200

    BAT and SH files updated for OpenJDK

---
+++ PointInPolygonJAR/start_JDK8.bat
@@ -0,0 +1,1 @@
+java -jar PointInPolygonMaven-1.0.jar mp3
---
+++ PointInPolygonJAR/start_OpenJFX.bat
@@ -0,0 +1,1 @@
+java --module-path "C:\Program Files\Java\javafx-sdk-13.0.2\lib" --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media -jar PointInPolygonMaven-1.0.jar mp3
