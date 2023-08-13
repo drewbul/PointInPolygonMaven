2ce3ed7c83639e66d07409a682687d6a1fb2eead
Andrew Buldyzhov <andrew@a-t-engineering.com>
2023-08-13 13:53:02 +0300

    Updated deploy scripts for JDK17

---
+++ PointInPolygonJAR/install_JavaFX.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget https://download2.gluonhq.com/openjfx/20.0.2/openjfx-20.0.2_linux-x64_bin-sdk.zip -O ~/Downloads/openjfx-sdk.zip
+sudo unzip ~/Downloads/openjfx-sdk.zip -d /usr/share/
+rm ~/Downloads/openjfx-sdk.zip
+sudo mv /usr/share/javafx-sdk-20.0.2/ /usr/share/openjfx/
+
+
+echo '
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/bin/java
+PATH_TO_FX=/usr/share/openjfx/lib
+' | sudo tee -a /etc/environment
+
+source /etc/environment
--- PointInPolygonJAR/start_OpenJFX.sh
+++ PointInPolygonJAR/start_OpenJFX.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media -jar PointInPolygonMaven-1.0.jar wav
+java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media -Djavafx.allowjs=true -jar PointInPolygonMaven-1.0.jar wav
